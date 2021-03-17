4aab563f17fce3d6bfc4aec0d04e69f968a30abc
MarioJRG <mariojrg.mjrg@gmail.com>
2021-03-17 01:44:31 -0700

    cambios para imagen

--- modulos/blogController.php
+++ modulos/blogController.php
@@ -11,10 +11,21 @@ class blog extends DB {
     private $imagen;
 
     function guardar( $autor, $titulo, $descripcion, $blog,$categoria,$fecha){
+        
+        
         $query = $this ->connect() -> prepare ('INSERT INTO blogs (autor,titulo,descripcion,blog,categoria,fecha) 
         VALUES(:autor,:titulo,:descripcion,:blog,:categoria,:fecha)');
-        $query ->execute(['autor'=>$autor,'titulo'=>$titulo,'descripcion'=>$descripcion,
-        'blog'=>$blog,'categoria'=>$categoria,'fecha'=>$fecha]);
+
+        $query ->bindParam(':autor',$autor,PDO::PARAM_STR);
+        $query ->bindParam(':titulo',$titulo,PDO::PARAM_STR);
+        $query ->bindParam(':descripcion',$descripcion,PDO::PARAM_STR);
+        $query ->bindParam(':blog',$blog,PDO::PARAM_STR);
+        $query ->bindParam(':categoria',$categoria,PDO::PARAM_STR);
+        $query ->bindParam(':fecha',$fecha,PDO::PARAM_STR);
+
+        $query->execute();
+       // $query ->execute(['autor'=>$autor,'titulo'=>$titulo,'descripcion'=>$descripcion,
+        //'blog'=>$blog,'categoria'=>$categoria,'fecha'=>$fecha]);
         if ($query) {
             return true;
         }  
